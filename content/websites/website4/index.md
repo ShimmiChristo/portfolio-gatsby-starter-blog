c9f27d8053c9b180fa212a77ea1e2571abb72532
Chris Shimmin <cshimmin@TCAdmins-MacBook-Pro-5.local>
2020-05-30 15:10:51 -0400

    VIDEO 10 - update gatsby-node.js file and quick bug fix

--- content/websites/website4/index.md
+++ content/websites/website4/index.md
@@ -1,6 +1,6 @@
 ---
 title: First Recipe!
-date: "2015-08-01
+date: "2015-08-01"
 description: "Hello World"
 category: website
 url: ./website-example.jpg
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,33 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
+  const blog = "blog";
+  const website = "website";
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const blogPost = path.resolve(`./src/templates/blog-post.js`);
   const result = await graphql(
     `
       {
-        allMarkdownRemark(
+        ${blog}: allMarkdownRemark(
+          filter: { frontmatter: { category: { eq: "${blog}" } } }
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+        ${website}: allMarkdownRemark(
+          filter: { frontmatter: { category: { eq: "${website}" } } }
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
@@ -25,18 +44,20 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     `
-  )
+  );
 
   if (result.errors) {
-    throw result.errors
+    throw result.errors;
   }
 
   // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges
+  const blogPosts = result.data[`${blog}`].edges;
+  const websitePosts = result.data[`${website}`].edges;
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+  blogPosts.forEach((post, index) => {
+    const previous =
+      index === blogPosts.length - 1 ? null : blogPosts[index + 1].node;
+    const next = index === 0 ? null : blogPosts[index - 1].node;
 
     createPage({
       path: post.node.fields.slug,
@@ -46,19 +67,65 @@ exports.createPages = async ({ graphql, actions }) => {
         previous,
         next,
       },
-    })
-  })
-}
+    });
+  });
+  websitePosts.forEach((post, index) => {
+    const previous =
+      index === websitePosts.length - 1 ? null : websitePosts[index + 1].node;
+    const next = index === 0 ? null : websitePosts[index - 1].node;
+
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
+  const blog = "blog";
+  const website = "website";
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+  if (
+    node.internal.type === `MarkdownRemark` &&
+    node.frontmatter.category === `${blog}`
+  ) {
+    // const value = createFilePath({ node, getNode });
     createNodeField({
       name: `slug`,
       node,
-      value,
-    })
+      value: `/${blog}${createFilePath({
+        node,
+        getNode,
+        basePath: `${blog}/`,
+        trailingSlash: false,
+      })}`,
+    });
+  } else if (
+    node.internal.type === `MarkdownRemark` &&
+    node.frontmatter.category === `${website}`
+  ) {
+    // const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value: `/${website}${createFilePath({
+        node,
+        getNode,
+        basePath: `${website}/`,
+        trailingSlash: false,
+      })}`,
+    });
+  } else if (node.internal.type === `MarkdownRemark`) {
+    createNodeField({
+      name: `slug`,
+      node,
+      value: `${createFilePath({ node, getNode, trailingSlash: false })}`,
+    });
   }
-}
+};
